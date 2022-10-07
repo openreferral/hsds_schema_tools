87a0c79f65b8fffdab933201626e2edf2570c97e
David Raznick <kindly@gmail.com>
2022-10-07 16:13:14 +0100

    new commands

--- hsds_schema.py
+++ hsds_schema.py
@@ -3,10 +3,13 @@
 import csv
 import os
 import json
+import copy
 import click
 import pathlib 
 import flatterer
 import glob
+import shutil
+import tempfile
 
 FILES = ['metadata', 'tables', 'fields', 'foreign_keys']
 
@@ -127,6 +130,219 @@ def clean_datapackage(datapackage):
 
     with open(datapackage, "w+") as f:
         datapackage_obj = json.dump(datapackage_obj, f, indent=4)
+
+
+
+@cli.command()
+@click.argument('datapackage')
+@click.argument('output_dir')
+def datapackage_to_schemas(datapackage, output_dir):
+
+    output_path = pathlib.Path(output_dir)
+    if output_path.exists():
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    with open(datapackage) as f:
+        datapackage_obj = json.load(f)
+    
+    all_refs = {}
+    to_process = ['service', 'attribute', 'organization', 'service_at_location', 'location', 'contact', 'phone']
+    all_fks = set()
+
+    for resource in datapackage_obj['resources']:
+        fks = resource["schema"].get("foreignKeys", [])
+        for fk in fks:
+            all_fks.add((resource["name"], fk["reference"]["resource"]))
+    
+    while to_process:
+        table = to_process.pop(0)
+        refs = []
+        for fk_table, reference_table in list(all_fks):
+            if table == fk_table:
+                refs.append((reference_table, "object"))
+                if reference_table not in to_process:
+                    to_process.append(reference_table)
+                all_fks.remove((fk_table, reference_table))
+            if table == reference_table:
+                refs.append((fk_table, "array"))
+                if fk_table not in to_process:
+                    to_process.append(fk_table)
+                all_fks.remove((fk_table, reference_table))
+        if refs:
+            all_refs[table] = refs
+    
+
+    for num, resource in enumerate(datapackage_obj['resources']):
+        json_schema_path = output_path / (resource['name'] + '.json')
+        resource_schema = copy.deepcopy(resource)
+        resource_schema["order"] = num + 1
+        resource_schema["type"] = "object"
+        resource_schema["properties"] = {}
+        fields = resource_schema["schema"].pop("fields")
+
+        required_list = []
+        tabular_required_list = []
+
+        for field in fields:
+            resource_schema["properties"][field['name']] = field
+            constraints = field.get('constraints', {})
+            required = constraints.pop('required', False)
+            if required:
+                if field['name'].endswith("_id") or field['name'] in ['link_entity', 'resource_type']:
+                    tabular_required_list.append(field['name'])
+                else:
+                    required_list.append(field['name'])
+
+            enum = constraints.pop('enum', False)
+            if enum:
+                field['enum'] = enum
+                
+        
+        if required_list:
+            resource_schema['required'] = required_list
+
+        if tabular_required_list:
+            resource_schema['tabular_required'] = tabular_required_list
+        
+        table_refs = all_refs.get(resource['name'])
+
+        if table_refs:
+            for table, relationship in table_refs:
+                if relationship == "object":
+                    resource_schema["properties"][f"{table}"] = {"name": f"{table}", "$ref": f"{table}.json"}
+                else:
+                    resource_schema["properties"][f"{table}s"] = {"name": f"{table}s", "type": "array", "items": {"$ref": f"{table}.json"}}
+
+        if resource['name'] not in ["attribute", "taxonomy", "taxonomy_term", "metadata"]:
+            resource_schema["properties"]["attributes"] = {"name": "attributes", "type": "array", "items": {"$ref": f"attribute.json"}}
+
+        if resource['name'] != "metadata":
+            resource_schema["properties"]["metadata"] = {"name": "metadata", "type": "array", "items": {"$ref": f"metadata.json"}}
+
+        resource_schema.pop("schema")
+
+        with open(json_schema_path, "w+") as f:
+            json.dump(resource_schema, f, indent=4)
+
+
+@cli.command()
+@click.argument('jsonschema_dir')
+def schemas_to_datapackage(jsonschema_dir):
+    input_path = pathlib.Path(jsonschema_dir)
+    
+    fks = []
+    schemas = []
+
+    for json_schema in input_path.glob("*.json"):
+        schema = json.loads(json_schema.read_text())
+        schemas.append(schema)
+        name = schema['name']
+        for field, prop in schema['properties'].items():
+            array_ref = prop.get('items', {}).get("$ref")
+            if array_ref and array_ref not in ['attribute.json', 'metadata.json']:
+                table = array_ref.replace('.json', '')
+                fks.append((table, name))
+            obj_ref = prop.get("$ref")
+            if obj_ref:
+                table = obj_ref.replace('.json', '')
+                fks.append((name, table))
+    
+    resources = []
+
+    for schema in sorted(schemas, key=lambda i: i['order']):
+        foreign_keys = []
+
+        required = []
+        required.extend(schema.get("required", []))
+        required.extend(schema.get("tabular_required", []))
+
+        schema.pop("order")
+
+        name = schema['name']
+
+        for table, foriegn_table in fks:
+            if table == name:
+                foreign_keys.append(
+                    {
+                        "fields": f"{foriegn_table}_id",
+                        "reference": {
+                            "resource": foriegn_table,
+                            "fields": "id"
+                        }
+                    }
+                )
+
+        if foreign_keys:
+            schema["foriegn_keys"] = foreign_keys
+        
+        fields = []
+
+        for field, prop in list(schema.pop('properties').items()):
+            contraints = prop.get('constraints')
+            if contraints:
+                prop['constraints']['required'] = field in required
+                fields.append(prop)
+        
+        schema.pop('required', None)
+        schema.pop('tabular_required', None)
+        
+        schema['schema'] = {"primaryKey": "id"} # {""}['fields'] = fields
+        schema['schema']['fields'] = fields
+
+        resources.append(schema)
+
+    datapackage = {
+        "name": "human_services_data",
+        "title": "Human Services Data Specification",
+        "description": "HSDS describes data about organizations, the services they provide, the locations at which these services can be accessed, and associated details.",
+        "profile": "tabular-data-package",
+        "version": "3.0.0",
+        "homepage": "http://docs.openreferral.org",
+        "license": {
+            "url": "https://creativecommons.org/licenses/by-sa/4.0/",
+            "type": "CC-BY-SA-4.0",
+            "name": "Creative Commons Attribution-ShareAlike 4.0"
+        },
+        "resources": resources
+    }
+
+    print(json.dumps(datapackage, indent=2))
+
+
+@cli.command()
+@click.argument('jsonschema_dir')
+def schemas_to_csv(jsonschema_dir):
+
+    input_path = pathlib.Path(jsonschema_dir)
+
+    def table_iterator():
+        schemas = []
+        for json_schema in input_path.glob("*.json"):
+            schema = json.loads(json_schema.read_text())
+            schemas.append(schema)
+
+        for schema in sorted(schemas, key=lambda i: i['order']):
+            name = schema['name']
+
+            required = schema.get("required", [])
+            tabular_required = schema.get("tabular_required", [])
+
+            for field, prop in list(schema.pop('properties').items()):
+                prop['table_name'] = name
+
+                contraints = prop.get('constraints')
+                if not contraints:
+                    prop['constraints'] = {}
+
+                prop['constraints']['required'] = field in required
+                prop['constraints']['tablular_required'] = field in tabular_required
+                yield prop
+    
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flatterer.flatten(table_iterator(), tmpdirname, force=True, fields_csv='fields_for_csv.csv', only_fields=True)
+        path = pathlib.Path(tmpdirname) / 'csv' / 'main.csv'
+        print(path.read_text())
 
 
 if __name__ == '__main__':

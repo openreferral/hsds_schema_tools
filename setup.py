4fdf152f3aa8cfecf4d79cc6e9319d72157b1bb5
David Raznick <kindly@gmail.com>
2023-02-08 11:12:41 +0000

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name="hsds_schema_tools",
-    version="0.0.2",
+    version="0.0.3",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     py_modules=["hsds_schema"],
